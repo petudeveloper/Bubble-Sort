9ce5749cbc366765ec7c289abf6515d39e77ecd6
petumazo <davidalvarezmazzo@gmail.com>
2021-04-21 18:34:34 -0500

    Modification to sort by algorith

--- bubblesort.rb
+++ bubblesort.rb
@@ -19,22 +19,24 @@ end
 ages = Array[1, 8, 9, 17, 6, 9, 5, 2]
 puts bubble_sort(ages)
 
-# sorted by algorithm
+#sorted by algorithm
 def bubble_sort_by(array)
-  index = 0
-  array_length = array.length - 1
-  while index < array_length
-    if yield(array[index], array[index + 1]).negative?
+  not_sorted = true
+  while not_sorted
+    index = 0
+    not_sorted = false
+    while index < (array.length - 1)
+      if yield(array[index], array[index + 1]).positive?
+        array[index], array[index + 1] = array[index + 1], array[index]
+        not_sorted = true
+      end
       index += 1
-    elsif yield(array[index], array[index + 1]).positive?
-      array[index], array[index + 1] = array[index + 1], array[index]
-      index = 0
     end
   end
   print array
 end
 
 # case scenario given
-bubble_sort_by(%w[hi hello hey poor abcderf]) do |left, right|
+bubble_sort_by(["hi","hello","hey", "poor", "abcderf"]) do |left,right|
   left.length - right.length
 end
