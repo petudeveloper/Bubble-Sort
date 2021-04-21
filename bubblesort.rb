9ba2ae8fbb0abf1cce5193c8fbb0ae2a7c9a7dea
Daniel Samuel <dansam5000@gmail.com>
2021-04-21 12:57:07 +0100

    add algorithm implementation of bubble sort

---
+++ bubblesort.rb
@@ -0,0 +1,19 @@
+def bubble_sort (array)
+    not_sorted = true
+    while not_sorted do
+        index = 0
+        not_sorted = false
+        while index < (array.length - 1)
+            if array[index] > array[index + 1]
+                array[index], array[index + 1] = array[index + 1], array[index]
+                not_sorted = true
+            end
+            index += 1
+        end
+    end
+    print array
+end
+
+# case scenario given
+ages = Array[1, 8, 9, 17, 6, 9, 5, 2]
+puts bubble_sort(ages)
