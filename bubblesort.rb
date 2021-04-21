5489637709c9cb07425a1ba9d371539a545f17d6
Daniel Samuel <dansam5000@gmail.com>
2021-04-21 21:26:37 +0100

    add bubble sort by algorithm

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,3 +1,4 @@
+# bubble sort algorithm
 def bubble_sort(array)
   not_sorted = true
   while not_sorted
@@ -17,3 +18,22 @@ end
 # case scenario given
 ages = Array[1, 8, 9, 17, 6, 9, 5, 2]
 puts bubble_sort(ages)
+
+#sorted by algorithm
+def bubble_sort_by(array)
+  not_sorted = true
+  while not_sorted do
+    index = 0
+    not_sorted = false
+    while index < array.length - 1
+      if(yield array[index], array[index + 1]).to_i >= 0
+        array[index], array[index + 1] = array[index + 1], array[index]
+      end
+      index += 1
+    end
+  end
+  print array
+end
+
+greetings = Array["hello", "hey", "hi"]
+puts bubble_sort_by(greetings)
